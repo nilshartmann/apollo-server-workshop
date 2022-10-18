31f864b72ec4007e20fb76752a18631ffcb0c09f
Nils Hartmann <nils@nilshartmann.net>
2022-10-18 17:04:00 +0200

    Testdaten

--- app/userservice/src/users.js
+++ app/userservice/src/users.js
@@ -3,4 +3,5 @@ module.exports = [
   { id: "U2", login: "larkin", fullname: "Brandt Larkin" },
   { id: "U3", login: "treutel", fullname: "Vita Treutel" },
   { id: "U4", login: "sue", fullname: "Sue Taylor" },
+  { id: "U5", login: "klaus", fullname: "Klaus Meier" },
 ];
